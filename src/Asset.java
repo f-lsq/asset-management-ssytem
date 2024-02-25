19bf09f2134770f9514ef6f5780f20a2d038fe4a
f-lsq <fionaloo69@gmail.com>
2024-02-26 02:01:04 +0800

    Added getter setter

---
+++ src/Asset.java
@@ -0,0 +1,31 @@
+public class Asset {
+    private String name;
+    private double cost;
+    private String id;
+
+    // Getters and Setters (since other classes cannot assess the private variables)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Asset asset1 = new Asset();
+        asset1.setName("XPS 17 Laptop");
+        asset1.setCost(450.00);
+        asset1.setId("A-00125");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Asset asset2 = new Asset();
+        asset2.setName("CP Plus CP-VAC-T50PL2-V2 5MP CCTV");
+        asset2.setCost(150.00);
+        asset2.setId("A-00342");
+
+        System.out.println("Asset1:");
+        System.out.println("Name - " + asset1.getName());
+        System.out.println("Cost - " + asset1.getCost());
+        System.out.println("Id - " + asset1.getId());
+
+        System.out.println("Asset2:");
+        System.out.println("Name - " + asset2.getName());
+        System.out.println("Cost - " + asset2.getCost());
+        System.out.println("Id - " + asset2.getId());
     }
 }
